ebdd925d1fb98d504ae4c12a55908fea16195942
“EnriqueColpo” <enriquecolpo@gmail.com>
2022-09-04 14:28:11 -0300

    Docs creado

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {
+    TodoList,
+    Todo
+} from "./classes"
+import { crearTodoHtml } from "./js/componentes";
+
+import "./styles.css";
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach(todo => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml);
+
